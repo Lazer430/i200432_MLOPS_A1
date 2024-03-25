de938a7ac70a0e41bada7bed68ed7ce0ecc47ab6
ahsanrbaloch <ahsanrasheed135@gmail.com>
2024-03-25 23:18:22 +0500

    fixed code quality

--- app/app.py
+++ app/app.py
@@ -12,14 +12,18 @@ model = load_model('model.h5')
 # Define the allowed file extensions for image uploads
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+
 # Function to check if the file extension is allowed
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Route for the homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # Route to handle image upload and prediction
 @app.route('/predict', methods=['POST'])
@@ -27,13 +31,14 @@ def predict():
     # Check if a file was uploaded
     if 'file' not in request.files:
         return "No file part"
-    
+
     file = request.files['file']
-    
-    # If user does not select file, browser also submit an empty part without filename
+
+# If user does not select file, browser
+# also submit an empty part without filename
     if file.filename == '':
         return "No selected file"
-    
+
     # If the file has an allowed extension
     if file and allowed_file(file.filename):
         image = Image.open(io.BytesIO(file.read()))
@@ -45,6 +50,7 @@ def predict():
 
     else:
         return "Invalid file format"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
