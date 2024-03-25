60894d36859592ecc6b16254092ec56e6c6bede9
Lazer430 <fasihabdullah430@gmail.com>
2024-03-25 23:30:40 +0500

    finished test code

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
 import pytest
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 @pytest.fixture(scope="module")
 def test_data():
@@ -16,5 +17,26 @@ def test_data():
 
 def test_accuracy(test_data):
     x_test, y_test = test_data
-    model = load_model('./app/model.pkl')
-    assert model.evaluate(x_test, y_test)[1] > 0.7
+    model = load_model('./app/model.h5')
+    assert model.evaluate(x_test, y_test)[1] > 0.5
+
+def test_precision(test_data):
+    x_test, y_test = test_data
+    model = load_model('./app/model.h5')
+    y_pred = model.predict(x_test)
+    y_pred = np.argmax(y_pred, axis=1)
+    assert precision_score(y_test, y_pred, average='micro') > 0.5
+
+def test_recall(test_data):
+    x_test, y_test = test_data
+    model = load_model('./app/model.h5')
+    y_pred = model.predict(x_test)
+    y_pred = np.argmax(y_pred, axis=1)
+    assert recall_score(y_test, y_pred, average='micro') > 0.5
+
+def test_f1(test_data):
+    x_test, y_test = test_data
+    model = load_model('./app/model.h5')
+    y_pred = model.predict(x_test)
+    y_pred = np.argmax(y_pred, axis=1)
+    assert f1_score(y_test, y_pred, average='micro') > 0.5
