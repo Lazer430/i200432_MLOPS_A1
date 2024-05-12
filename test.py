c02521df12e74f84640541691d07c9208faeff10
Muhammad Ahsan Rasheed <72220980+ahsanrbaloch@users.noreply.github.com>
2024-05-12 15:18:58 +0500

    formatted test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-# test_app.py
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
 import pytest
 from sklearn.metrics import precision_score, recall_score, f1_score
+
 
 @pytest.fixture(scope="module")
 def test_data():
@@ -15,10 +14,12 @@ def test_data():
     x_test = x_test.reshape(-1, 28, 28, 1)
     return x_test, y_test
 
+
 def test_accuracy(test_data):
     x_test, y_test = test_data
     model = load_model('./app/model.h5')
     assert model.evaluate(x_test, y_test)[1] > 0.5
+
 
 def test_precision(test_data):
     x_test, y_test = test_data
@@ -27,12 +28,14 @@ def test_precision(test_data):
     y_pred = np.argmax(y_pred, axis=1)
     assert precision_score(y_test, y_pred, average='micro') > 0.5
 
+
 def test_recall(test_data):
     x_test, y_test = test_data
     model = load_model('./app/model.h5')
     y_pred = model.predict(x_test)
     y_pred = np.argmax(y_pred, axis=1)
     assert recall_score(y_test, y_pred, average='micro') > 0.5
+
 
 def test_f1(test_data):
     x_test, y_test = test_data
